de6ed8050d13b8f7d4f023f21ffcd9596426503e
Siberyanhusky <beryan26y7@gmail.com>
2026-06-27 21:26:20 +0700

    Create materi.js

---
+++ js/materi.js
@@ -0,0 +1,16 @@
+const chatAI = document.getElementById("chatAI");
+const lihatAR = document.getElementById("lihatAR");
+
+// Tombol AI
+chatAI.addEventListener("click",()=>{
+
+    alert("AI Tutor akan segera tersedia.");
+
+});
+
+// Tombol AR
+lihatAR.addEventListener("click",()=>{
+
+    window.location.href="../ar/";
+
+});
